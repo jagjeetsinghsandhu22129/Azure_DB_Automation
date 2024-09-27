50b09f8f6557dab85f33a54702e487fe00dd18bd
Jagjeet Singh <jagjeetsinghsandhu22129@gmail.com>
2024-09-27 17:01:11 -0400

    changes in sql file

--- schema_changes_git.sql
+++ schema_changes_git.sql
@@ -8,3 +8,14 @@ CREATE TABLE IF NOT EXISTS employees (
 
 -- Add a new column to an existing table
 ALTER TABLE employees ADD COLUMN email VARCHAR(100);
+
+
+--Insert some data in the table
+
+-- Insert data into the employees table
+INSERT INTO employees (Name, Department, Hire_date, email) 
+VALUES 
+    ('John', 'Marketing', '2021-06-15', 'john@example.com'),
+    ('Rahul', 'Engineering', '2020-09-25', 'rahul@example.com'),
+    ('Mandeep Singh', 'Finance', '2019-03-12', 'mandeep.s@example.com'),
+    ('Mukesh Kumar', 'Human Resources', '2022-01-10', 'mukesh.k@example.com');
