15c341a2e8b0d6a38c8f8239afb948a464db3aea
Jagjeet Singh <jagjeetsinghsandhu22129@gmail.com>
2024-09-27 17:36:25 -0400

    changes in sql file

--- schema_changes_git.sql
+++ schema_changes_git.sql
@@ -1,10 +1,10 @@
 -- Add a new table
-CREATE TABLE IF NOT EXISTS employees (
-    Id SERIAL PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Department VARCHAR(50) NOT NULL,
-    Hire_date DATE NOT NULL
-);
+--CREATE TABLE IF NOT EXISTS employees (
+   -- Id SERIAL PRIMARY KEY,
+   -- Name VARCHAR(100) NOT NULL,
+   -- Department VARCHAR(50) NOT NULL,
+    --Hire_date DATE NOT NULL
+--);
 
 -- Add a new column to an existing table
 --ALTER TABLE employees ADD COLUMN email VARCHAR(100);
