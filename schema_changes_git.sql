238b3bc238c2b175e10975630c1301ddf2f23507
Jagjeet Singh <jagjeetsinghsandhu22129@gmail.com>
2024-09-27 17:31:48 -0400

    changes in sql file

--- schema_changes_git.sql
+++ schema_changes_git.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS employees (
     Name VARCHAR(100) NOT NULL,
     Department VARCHAR(50) NOT NULL,
     Hire_date DATE NOT NULL
---);
+);
 
 -- Add a new column to an existing table
 ALTER TABLE employees ADD COLUMN email VARCHAR(100);
