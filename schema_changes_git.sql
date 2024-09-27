c6529b0fbe6498921642eb323fd7b85ebcb269eb
Jagjeet Singh <jagjeetsinghsandhu22129@gmail.com>
2024-09-27 17:20:31 -0400

    changes in sql file

--- schema_changes_git.sql
+++ schema_changes_git.sql
@@ -1,13 +1,13 @@
 -- Add a new table
-CREATE TABLE IF NOT EXISTS employees (
-    Id SERIAL PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Department VARCHAR(50) NOT NULL,
-    Hire_date DATE NOT NULL
-);
+--CREATE TABLE IF NOT EXISTS employees (
+   -- Id SERIAL PRIMARY KEY,
+   -- Name VARCHAR(100) NOT NULL,
+   -- Department VARCHAR(50) NOT NULL,
+    --Hire_date DATE NOT NULL
+--);
 
 -- Add a new column to an existing table
-ALTER TABLE employees ADD COLUMN email VARCHAR(100);
+--ALTER TABLE employees ADD COLUMN email VARCHAR(100);
 
 
 --Insert some data in the table
@@ -15,7 +15,7 @@ ALTER TABLE employees ADD COLUMN email VARCHAR(100);
 -- Insert data into the employees table
 INSERT INTO jsfirstdb.employees (Name, Department, Hire_date, email) 
 VALUES 
-    ('John', 'Marketing', '2021-06-15', 'john@example.com')--,
-    --('Rahul', 'Engineering', '2020-09-25', 'rahul@example.com'),
-    --('Mandeep Singh', 'Finance', '2019-03-12', 'mandeep.s@example.com'),
-    --('Mukesh Kumar', 'Human Resources', '2022-01-10', 'mukesh.k@example.com');
+    ('John', 'Marketing', '2021-06-15', 'john@example.com'),
+    ('Rahul', 'Engineering', '2020-09-25', 'rahul@example.com'),
+    ('Mandeep Singh', 'Finance', '2019-03-12', 'mandeep.s@example.com'),
+    ('Mukesh Kumar', 'Human Resources', '2022-01-10', 'mukesh.k@example.com');
