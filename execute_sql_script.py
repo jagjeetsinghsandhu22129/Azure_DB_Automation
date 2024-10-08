1c5324829cdf3d358ce9ae95c6808cfed3976079
Jagjeet Singh <jagjeetsinghsandhu22129@gmail.com>
2024-10-08 19:20:55 -0400

    Update execute_sql_script.py

--- execute_sql_script.py
+++ execute_sql_script.py
@@ -12,7 +12,7 @@ connection = mysql.connector.connect(
 cursor = connection.cursor()
 
 # Open and execute the SQL script
-with open('update_companydb_schema.sql', 'r') as file:
+with open('add_departments.sql', 'r') as file:
     sql_script = file.read()
     sql_commands = sql_script.split(';')
     
